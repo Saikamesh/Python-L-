1474e729f156583150da7435acbdfe7748fd7592
Sai1919 <51651413+Saikamesh@users.noreply.github.com>
2020-05-25 12:27:37 +0530

    First Python commit

---
+++ app.py
@@ -0,0 +1,8 @@
+weight = int(input('Enter your Weight'))
+unit = input('(L)bs or (K)g:')
+if unit.upper() == 'L':
+    converter = weight *0.45
+    print("You are {} Kilos".format(converter))
+else:
+    converter = weight / 0.45 
+    print("You are {} Pounds".format(converter))
